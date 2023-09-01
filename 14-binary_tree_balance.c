2dcd5393adaaf81278d6725c62740630a79a3867
craymahdee142 <craymahdaniel@gmail.com>
2023-08-31 19:57:43 -0700

    binary_trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,10 +7,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
-
-	if (tree != NULL)
+	if (tree != NUL)
 	{
+		size_t left_height = 0, right_height = 0;
+
 		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((left_height > right_height) ? left_height : right_height);
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || node->right != NULL || node->left != NULL
+	if (node == NULL || node->left != NULL || node->right != NULLi)
 		return (0);
 	return (1);
 }
